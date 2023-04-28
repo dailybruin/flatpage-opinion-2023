0271b29f0b16ab38481da2b24925a033c846cb46
jainsujay02 <sujayjain2121@gmail.com>
2023-04-28 11:05:29 -0700

    Added background image
    - imported background.svg
    - displayed background.svg as background for all components called under app.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import background from './images/background.svg'
 
 function App() {
   const [ data, setData ] = useState(null);
-  
+
   useEffect(() => {
 		fetch("https://kerckhoff.dailybruin.com/api/packages/flatpages/flatpage-opinion-2023")
 		.then(res => res.json())
@@ -14,7 +15,7 @@ function App() {
   }, [])
 
   return data && (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})`}}>
       <Header/>
       Hello Daily Bruin!
       <Footer/>
