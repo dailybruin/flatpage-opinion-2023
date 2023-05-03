0570a9b25b2f9a1d37df26390924a9242322cc0f
Yifan Tang <yifant@g.ucla.edu>
2023-05-02 19:21:26 -0700

    added landing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { useState, useEffect } from "react";
-import styled from 'styled-components';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import styled from "styled-components";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Landing from "./components/landing";
 
 function App() {
-  const [ data, setData ] = useState(null);
-  
-  useEffect(() => {
-		fetch("https://kerckhoff.dailybruin.com/api/packages/flatpages/flatpage-opinion-2023")
-		.then(res => res.json())
-		.then(res => setData(res.data['article.aml']))
-  }, [])
+  const [data, setData] = useState(null);
 
-  return data && (
-    <div className="App">
-      <Header/>
-      Hello Daily Bruin!
-      <Footer/>
-    </div>
+  useEffect(() => {
+    fetch(
+      "https://kerckhoff.dailybruin.com/api/packages/flatpages/flatpage-opinion-2023"
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res.data["article.aml"]));
+  }, []);
+
+  return (
+    data && (
+      <div className="App">
+        <Header />
+        Hello Daily Bruin!
+        <Landing
+          landing_image={data.landing_image}
+          landing_image_static={data.landing_image_static}
+          landing_credits={data.landing_credits}
+        />
+        <Footer />
+      </div>
+    )
   );
 }
 
